d49a8a80090dcf9aadaf62a9744308ab9c367d4d
Vadim Cherepenichev <vadim.cvy@gmail.com>
2021-08-22 03:24:02 +0300

    Implement plugin settings controller

---
+++ inc/plugin-settings/Plugin_Settings.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Cvy_AC\inc\plugin_settings;
+
+/**
+ * Plugin settings controller.
+ */
+class Plugin_Settings
+{
+    use \Cvy_AC\helpers\inc\design_pattern\tSingleton;
+
+    protected function __construct()
+    {
+        $this->register_settings();
+
+        $this->register_settings_pages();
+    }
+
+    /**
+     * Registers plugin settings (options).
+     *
+     * @return void
+     */
+    protected function register_settings() : void
+    {
+        \Cvy_AC\inc\plugin_settings\settings\general\Setting::get_instance();
+    }
+
+    /**
+     * Registers plugin settings pages.
+     *
+     * @return void
+     */
+    protected function register_settings_pages() : void
+    {
+        \Cvy_AC\inc\plugin_settings\pages\Main::get_instance();
+    }
+}
