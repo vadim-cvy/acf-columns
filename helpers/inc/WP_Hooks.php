25f6e642438b16e179acaa74b0bcfdebd9d9a094
Vadim Cherepenichev <vadim.cvy@gmail.com>
2021-08-29 02:45:31 +0300

    Add ability to add filters safely

--- helpers/inc/WP_Hooks.php
+++ helpers/inc/WP_Hooks.php
@@ -25,4 +25,23 @@ class WP_Hooks
 
         add_action( $action_name, $callback );
     }
+
+    /**
+     * Makes sure filter hasn't fired before add_filter() is called.
+     *
+     * Wrapper for add_filter().
+     *
+     * @param string $filter_name
+     * @param callable  $callback
+     * @return void
+     */
+    public static function add_filter_ensure( string $filter_name, callable $callback ) : void
+    {
+        if ( did_action( $filter_name ) )
+        {
+            throw new \Exception( 'Can\'t handle add_filter()! Filter "' . $filter_name . '" has already fired.' );
+        }
+
+        add_filter( $filter_name, $callback );
+    }
 }
