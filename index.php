123a78f860f45a9e3fbced80670bbd40276d445c
Vadim Cherepenichev <vadim.cvy@gmail.com>
2021-09-02 16:50:45 +0300

    Add missed index.php files

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Plugin Name:       Cvy ACF Columns
- * Description:       This plugins makes it possible to add ACF fields as columns to post types, users and taxonomies tables in WP Dashboard.
- * Version:           1.0.0
- * Author:            Vadim Cherepenichev
- * Author URI:        https://www.toptal.com/resume/vadim-cherepenichev
- * Text Domain:       cvy-acf-columns
- */
