71a4e7a0df4c571b712bd69ddde0cd1834d33837
Vadim Cherepenichev <vadim.cvy@gmail.com>
2021-09-03 02:03:42 +0300

    Update plugin name and description

--- Plugin.php
+++ Plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name:       Cvy ACF Columns
- * Description:       This plugins makes it possible to add ACF fields as columns to post types, users and taxonomies tables in WP Dashboard.
+ * Plugin Name:       ACF Columns
+ * Description:       This plugins makes it possible to create dashboard table columns for ACF fields.
  * Version:           1.0.0
  * Author:            Vadim Cherepenichev
  * Author URI:        https://www.toptal.com/resume/vadim-cherepenichev
